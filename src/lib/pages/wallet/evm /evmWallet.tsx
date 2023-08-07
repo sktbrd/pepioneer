c05d06c75f9804a8d837e4560fc56a992d108b70
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-08-06 23:27:04 -0300

    send token modal

--- src/lib/pages/wallet/evm /evmWallet.tsx
+++ src/lib/pages/wallet/evm /evmWallet.tsx
@@ -1,5 +1,3 @@
-export {};
-
 import React, { useEffect, useState } from "react";
 import { usePioneer } from "lib/context/Pioneer";
 import {
@@ -16,13 +14,53 @@ import {
   Button,
   Text
 } from "@chakra-ui/react";
+import EvmSendModal from "./evmSendModal";
 
-interface Balance {
+interface TokenInfo {
+  id: string;
+  networkId: number;
+  address: string;
+  label: string;
+  name: string;
+  balance: string;
+  balanceUSD: string;
+  caip: string;
+  canExchange: boolean;
+  coingeckoId: string;
+  context: string;
+  contract: string;
+  createdAt: string;
+  dailyVolume: number;
+  decimals: number;
+  description: string;
+  explorer: string;
+  externallyVerified: boolean;
+  hide: boolean;
+  holdersEnabled: boolean;
+  image: string;
+  isToken: boolean;
+  lastUpdated: number;
+  marketCap: number;
+  network: string;
+  price: number;
+  priceUpdatedAt: string;
+  protocal: string;
+  pubkey: string;
+  source: string;
+  status: string;
+  symbol: string;
+  totalSupply: string;
+  updatedAt: string;
+  verified: boolean;
+  website: string;
+}
+
+interface EvmBalanceItem {
   id: string;
   image: string;
   name: string;
   balance: string;
-  balanceUSD: string;
+  balanceUSD: string; // Change the type to string
   network: string;
   pubkey: string;
 }
@@ -33,28 +71,29 @@ const EvmBalance: React.FC = (): JSX.Element => {
 
   const headers = ["Asset", "Balance", "Balance USD"];
 
-  const [tableData, setTableData] = useState<Balance[]>(user?.balances || []);
+  const [tableData, setTableData] = useState<EvmBalanceItem[]>(user?.balances || []);
   const [selectedBlockchain, setSelectedBlockchain] = useState<string>("all");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedTokenInfo, setSelectedTokenInfo] = useState<TokenInfo | null>(null);
 
   useEffect(() => {
     if (user?.balances) {
       if (selectedBlockchain === "all") {
         setTableData(user.balances);
       } else {
-        setTableData(user.balances.filter((balance: Balance) => balance.network === selectedBlockchain));
+        setTableData(user.balances.filter((balance: EvmBalanceItem) => balance.network === selectedBlockchain));
       }
     }
   }, [selectedBlockchain, user?.balances]);
 
-  const blockchains: string[] = Array.from(new Set(user?.balances?.map((balance: Balance) => balance.network))) || [];
-  blockchains.unshift("all");   
+  const blockchains: string[] = Array.from(new Set(user?.balances?.map((balance: EvmBalanceItem) => balance.network))) || [];
+  blockchains.unshift("all");
 
   const totalBalanceUSD = tableData.reduce(
     (total, balance) => total + parseFloat(balance.balanceUSD || "0"),
     0
   );
 
-  // Function to copy the receiving address to the clipboard
   const copyToClipboard = (address: string): void => {
     const textarea = document.createElement("textarea");
     textarea.value = address;
@@ -62,25 +101,17 @@ const EvmBalance: React.FC = (): JSX.Element => {
     textarea.select();
     document.execCommand("copy");
     document.body.removeChild(textarea);
-    alert( address +" copied to clipboard!");
+    alert(address + " copied to clipboard!");
   };
-  
+
+  const handleSendButtonClick = (tokenInfo: TokenInfo): void => {
+    setSelectedTokenInfo(tokenInfo);
+    setIsModalOpen(true);
+  };
 
   return (
-    <Box 
-      className="hive_box" 
-      borderRadius="12px" 
-      border="1px solid limegreen" 
-      padding="10px" 
-      overflow="auto" 
-      fontFamily="'Courier New', monospace">
-      <Text 
-        textAlign="center" 
-        borderRadius="12px" 
-        fontWeight="700" 
-        fontSize="18px" 
-        color="limegreen" 
-        padding="10px">
+    <Box className="hive_box" borderRadius="12px" border="1px solid limegreen" padding="10px" overflow="auto" fontFamily="'Courier New', monospace">
+      <Text textAlign="center" borderRadius="12px" fontWeight="700" fontSize="18px" color="limegreen" padding="10px">
         EVM Balance
       </Text>
 
@@ -89,12 +120,7 @@ const EvmBalance: React.FC = (): JSX.Element => {
           <h2>Total Estimated Balance of EVMs</h2>
           <p>{totalBalanceUSD.toFixed(2)} USD</p>
         </Box>
-        <Select
-          value={selectedBlockchain}
-          onChange={(e) => setSelectedBlockchain(e.target.value)}
-          ml="auto"
-          w="150px"
-        >
+        <Select value={selectedBlockchain} onChange={(e) => setSelectedBlockchain(e.target.value)} ml="auto" w="150px">
           {blockchains.map((blockchain) => (
             <option key={blockchain} value={blockchain as string}>
               {blockchain === "all" ? "All Blockchains" : blockchain}
@@ -122,20 +148,21 @@ const EvmBalance: React.FC = (): JSX.Element => {
                   <Image src={balance.image} alt={balance.name} boxSize="20px" mr="2" />
                   {balance.name}
                 </Td>
-                {/* Use the toFixed method to limit the decimals */}
                 <Td>{parseFloat(balance.balance).toFixed(2)}</Td>
                 <Td>{parseFloat(balance.balanceUSD).toFixed(2)}</Td>
                 <Td>
                   <Button onClick={() => copyToClipboard(balance.pubkey)}>Receive</Button>
                 </Td>
                 <Td>
-                  <Button onClick={() => console.log("clicked")}>Send</Button>
+                  <Button onClick={() => handleSendButtonClick(balance as TokenInfo)}>Send</Button>
                 </Td>
               </Tr>
             ))}
           </Tbody>
         </Table>
       )}
+
+      <EvmSendModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} tokenInfo={selectedTokenInfo} />
     </Box>
   );
 };
